4bbf7cd73099922a2305338ddd32e406fabc3eeb
kyrosle <le@90e.com>
2023-09-29 12:52:47 +0800

    修改 生成 protobuf rust代码的build脚本，设置为代码生成到源代码目录而非out_dir下，方便阅览代码

--- libs/hbb_common/build.rs
+++ libs/hbb_common/build.rs
@@ -1,14 +1,24 @@
+use std::process::Command;
+
 fn main() {
-    let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
+    // let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
 
-    std::fs::create_dir_all(&out_dir).unwrap();
+    // std::fs::create_dir_all(&out_dir).unwrap();
 
-    protobuf_codegen::Codegen::new()
-        .pure()
+    let out_dir = "src/protos";
+
+    protobuf_codegen::Codegen::new() // 创建代码生成器
+        .pure() // 使用rust的，不依赖protoc
         .out_dir(out_dir)
-        .inputs(["protos/rendezvous.proto", "protos/message.proto"])
-        .include("protos")
-        .customize(protobuf_codegen::Customize::default().tokio_bytes(true))
+        .inputs(["protos/rendezvous.proto", "protos/message.proto"]) // protobuf 协议文件
+        .include("protos") // 设置 protos 目录作为包含 .proto 文件的文件夹
+        .customize(protobuf_codegen::Customize::default().tokio_bytes(true)) // 支持 tokio::Bytes 类型
         .run()
         .expect("Codegen failed.");
+    Command::new("cargo")
+        .args(["fmt"])
+        .current_dir("src/pb")
+        .output()
+        .unwrap();
+    println!("cargo:rerun-if-changed=protos/*.proto");
 }
